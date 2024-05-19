d83fdffe2f948547f1a4f5f37b71b8d4cba4f88a
Jorge Saud <jsaud@modyo.com>
2024-05-19 18:06:08 -0400

    feat: add security headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,41 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import { shield } from '@kindspells/astro-shield'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://giorgiosaud.io",
   integrations: [tailwind(), mdx(), sitemap(),icon({
     iconDir: "src/components/ui/icons",
     include:{
       uil:['*'],
       bx:['*'],
       'simple-icons':['*'],
+    }
+  }),
+  shield({
+    sri: {
+      enableMiddleware: true,   // MUST be enabled!
+      hashesModule: modulePath, // SHOULD be set!
+    },
+
+    // - If set, it controls how the security headers will be generated in the
+    //   middleware.
+    // - If not set, no security headers will be generated in the middleware.
+    securityHeaders: {
+      // - If set, it controls how the CSP (Content Security Policy) header will
+      //   be generated in the middleware.
+      // - If not set, no CSP header will be generated in the middleware.
+      contentSecurityPolicy: {
+        // - If set, it controls the "default" CSP directives (they can be
+        //   overriden at runtime).
+        // - If not set, the middleware will use a minimal set of default
+        //   directives.
+        cspDirectives: {
+          'default-src': "'self' https://cdn.giorgiosaud.io",
+        }
+      }
     }
   })],
 });
