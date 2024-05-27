521a50f39aada81bcf6b620b516e253a0a0a086d
Jorge Saud <jsaud@modyo.com>
2024-05-27 11:03:43 -0400

    feat: add sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,22 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-import { resolve } from 'node:path'
+import { resolve } from 'node:path';
+const rootDir = new URL('.', import.meta.url).pathname;
+const modulePath = resolve(rootDir, 'src', 'generated', 'sriHashes.mjs');
 
-const rootDir = new URL('.', import.meta.url).pathname
-const modulePath = resolve(rootDir, 'src', 'generated', 'sriHashes.mjs')
 // https://astro.build/config
 export default defineConfig({
   site: "https://giorgiosaud.io",
   security: {
     checkOrigin: true
   },
-  integrations: [tailwind(), mdx(), sitemap(),icon({
+  integrations: [tailwind(), mdx(), sitemap(), icon({
     iconDir: "src/components/ui/icons",
-    include:{
-      uil:['*'],
-      bx:['*'],
-      'simple-icons':['*'],
+    include: {
+      uil: ['*'],
+      bx: ['*'],
+      'simple-icons': ['*']
     }
-  }),
-
- ],
+  })]
 });
