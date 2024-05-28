60f27b3be1e0bdf48adbc7602779e1438be98867
Jorge Saud <jsaud@modyo.com>
2024-05-28 12:04:50 -0400

    feat: add paginator

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,10 +10,21 @@ const modulePath = resolve(rootDir, 'src', 'generated', 'sriHashes.mjs');
 // https://astro.build/config
 export default defineConfig({
   site: "https://giorgiosaud.io",
+  redirects: {
+    '/notebook': {
+      status: 302,
+      destination: '/notebook/1'
+    }
+  },
   security: {
     checkOrigin: true
   },
-  integrations: [tailwind(), mdx(), sitemap(), icon({
+  integrations: [tailwind(), mdx(), sitemap({
+    entryLimit: 10000,
+    changefreq: 'weekly',
+    priority: 0.7,
+    lastmod: new Date('2022-02-24'),
+  }), icon({
     iconDir: "src/components/ui/icons",
     include: {
       uil: ['*'],
