4a07929de9d622247ec1b6b3fba1aa8ee2b6bbe0
Jorge Saud <jsaud@modyo.com>
2024-05-28 17:58:35 -0400

    fix: cloudflare deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import { resolve } from 'node:path';
+import cloudflare from "@astrojs/cloudflare";
 const rootDir = new URL('.', import.meta.url).pathname;
 const modulePath = resolve(rootDir, 'src', 'generated', 'sriHashes.mjs');
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,14 +20,16 @@ export default defineConfig({
     entryLimit: 10000,
     changefreq: 'weekly',
     priority: 0.7,
-    lastmod: new Date('2022-02-24'),
-  }), 
-  icon({
+    lastmod: new Date('2022-02-24')
+  }), icon({
     iconDir: "src/components/ui/icons",
     include: {
       uil: ['*'],
       bx: ['*'],
       'simple-icons': ['*']
     }
-  })]
+  })],
+  adapter: cloudflare({
+    imageService: 'cloudflare'
+  })
 });
