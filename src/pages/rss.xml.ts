39c230d85e1b909898ef42902a305645ce99aa80
Jorge Saud <jsaud@modyo.com>
2024-07-03 10:47:25 -0400

    feat: add rss feed

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,20 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const notes = await getCollection("notes");
+  return rss({
+    title: 'Giorgiosaud Notebook',
+    description: 'A developer notebook of important things',
+    site: context.site,
+    items: notes.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.publishDate,
+      description: post.data.resume,
+      // customData: post.data.customData,
+      // Compute RSS link from post `slug`
+      // This example assumes all posts are rendered as `/blog/[slug]` routes
+      link: `/notebook/${post.slug}/`,
+    })),
+  });
+}
