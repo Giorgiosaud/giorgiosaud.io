10d73ea7264a80ec446868cb56e8d362e7eec559
Jorge Saud <jsaud@modyo.com>
2024-07-23 12:52:35 -0400

    fix: codeblock and add copy text

--- astro.config.mjs
+++ astro.config.mjs
@@ -54,5 +54,27 @@ export default defineConfig({
   image: {
     domains: ['https://res.cloudinary.com']
   },
-  adapter: vercel()
+  adapter: vercel(),
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://shiki.style/themes
+      theme: 'vitesse-dark',
+      // Alternatively, provide multiple themes
+      // See note below for using dual light/dark themes
+      // Disable the default colors
+      // https://shiki.style/guide/dual-themes#without-default-color
+      // (Added in v4.12.0)
+      defaultColor: false,
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://shiki.style/languages
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+      // Add custom transformers: https://shiki.style/guide/transformers
+      // Find common transformers: https://shiki.style/packages/transformers
+      transformers: [],
+    },
+  },
 });
