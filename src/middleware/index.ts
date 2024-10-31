5d734fe8fc11ea2ecda966bd94b528a27d018260
Jorge Saud <jsaud@modyo.com>
2024-10-31 10:00:13 -0300

    fix: middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,25 +1,22 @@
-// import type { MiddlewareNext } from "astro";
-// import { getCollection, type ContentCollectionKey } from "astro:content";
-// import { defineMiddleware, sequence } from "astro:middleware";
-// const selfHealingMiddleware=(path:string,collection:ContentCollectionKey)=>async (_,next:MiddlewareNext)=>{
-//     const {url}=_;
-//     if(url.pathname.includes(`/${path}/`)){
-//         const requestedSlug=url.pathname.split("/").pop();
-//         const entriesSlugs = (await getCollection(collection, ({slug }) => {
-//             return slug;
-//         })).map((entry)=>entry.id);
-//         const hashToHeal = requestedSlug.match(/\d{6}/)?.[0]; 
-//         if(!entriesSlugs.includes(requestedSlug) && hashToHeal){
-            
-//             const slugToHeal=entriesSlugs.find((slug)=>{
-//                 return slug.includes(hashToHeal);
-//             })
-//             const redirectUrl = `${url.origin}/${path}/${slugToHeal}`;
-//             return Response.redirect(redirectUrl,301);
-//         }
-//     }
-//     const response = await next();
-//     return response;
-// }
+import type { MiddlewareNext } from "astro";
+import { getCollection, type ContentCollectionKey } from "astro:content";
+import { sequence } from "astro:middleware";
+const selfHealingMiddleware=(path:string,collection:ContentCollectionKey)=>async (_,next:MiddlewareNext)=>{
+    const {url}=_;
+    const splittedUrl = url.pathname.split("/");
+    if(url.pathname.includes(`/${path}/`) && splittedUrl.length>2){
+        const requestedSlug=splittedUrl.pop();
+        const hashToHeal = requestedSlug.match(/\d{6}/)?.[0]; 
+        const entrySlug = ((await getCollection(collection, ({id }) => {
+            return id;
+        })).map((entry)=>entry.id).find((slug)=>slug.includes(hashToHeal)))
+        if(entrySlug && entrySlug!==requestedSlug){
+            const redirectUrl = `${url.origin}/${path}/${entrySlug}`;
+            return Response.redirect(redirectUrl,301);
+        }
+    }
+    const response = await next();
+    return response;
+}
 
-// //export const onRequest = sequence(selfHealingMiddleware('notebook','notes'),selfHealingMiddleware('es/cuaderno','notas'))
+export const onRequest = sequence(selfHealingMiddleware('notebook','notes'),selfHealingMiddleware('es/cuaderno','notas'))
