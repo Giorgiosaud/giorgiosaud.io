757003aa9f48d0837230e1a6a4273ade5cca484d
Jorge Saud <jsaud@modyo.com>
2024-11-01 17:00:02 -0300

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,9 @@ import { sequence } from "astro:middleware";
 const selfHealingMiddleware=(path:string,collection:ContentCollectionKey)=>async (_,next:MiddlewareNext)=>{
     const {url}=_;
     const splittedUrl = url.pathname.split("/");
-    if(url.pathname.includes(`/${path}/`) && splittedUrl.length>2){
-        const requestedSlug=splittedUrl.pop();
-        const hashToHeal = requestedSlug.match(/\d{6}/)?.[0]; 
+    const requestedSlug=splittedUrl.pop();
+    const hashToHeal = requestedSlug.match(/\d{6}/)?.[0]; 
+    if(url.pathname.includes(`/${path}/`) && hashToHeal){
         const entrySlug = ((await getCollection(collection, ({id }) => {
             return id;
         })).map((entry)=>entry.id).find((slug)=>slug.includes(hashToHeal)))
