84b27a84b683b33ec42f9cdce5b322b221da557e
Jorge Saud <jsaud@modyo.com>
2024-11-01 23:52:27 -0300

    test: remove vercel analitycs

--- astro.config.mjs
+++ astro.config.mjs
@@ -70,7 +70,7 @@ export default defineConfig({
   },
   output: "static",
   adapter: vercel({
-    webAnalytics: { enabled: true },
+    // webAnalytics: { enabled: true },
     isr: true,
 
   }),
