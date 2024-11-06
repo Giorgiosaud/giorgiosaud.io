8eae1b127603c4f12fd6d220a6443221690edeae
Jorge Saud <jsaud@modyo.com>
2024-11-05 22:41:16 -0300

    fix: add tests

--- e2e/translatoins.test.ts
+++ e2e/translatoins.test.ts
@@ -1,13 +1,32 @@
 import { test, expect } from '@playwright/test';
 
-test('self healing works with existing healing path', async ({ page }) => {
+test('translation path works with existing healing path', async ({ page }) => {
   await page.goto("http://localhost:4321/notebook/000004-tag-link");
   await page.getByText('Español').click();
-  // await page.getByText('English');
   expect(page.url()).toBe("http://localhost:4321/es/cuaderno/000004-image-tag-link");
-});
-test('self healing works with existing healing path in spanish', async ({ page }) => {
-  await page.goto("http://localhost:4321/es/cuaderno/000004-image-tag-link");
   await page.getByText('English').click();
   expect(page.url()).toBe("http://localhost:4321/notebook/000004-tag-link");
+
 });
+test('translation path works with "/" path', async ({ page }) => {
+  await page.goto("http://localhost:4321/");
+  await page.getByText('Español').click();
+  expect(page.url()).toBe("http://localhost:4321/es");
+  await page.getByText('English').click();
+  expect(page.url()).toBe("http://localhost:4321/");
+});
+test('translation path works with about path', async ({ page }) => {
+  await page.goto("http://localhost:4321/about");
+  await page.getByText('Español').click();
+  expect(page.url()).toBe("http://localhost:4321/es/acerca-de-mi");
+  await page.getByText('English').click();
+  expect(page.url()).toBe("http://localhost:4321/about");
+});
+test('translation path works with contact path', async ({ page }) => {
+  await page.goto("http://localhost:4321/contact");
+  await page.getByText('Español').click();
+  expect(page.url()).toBe("http://localhost:4321/es/contactame");
+  await page.getByText('English').click();
+  expect(page.url()).toBe("http://localhost:4321/contact");
+});
+
