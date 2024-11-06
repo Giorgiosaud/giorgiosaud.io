bea40f88dcdcdfd744b3b0a9e87407feaabaa0f8
Jorge Saud <jsaud@modyo.com>
2024-11-05 22:37:00 -0300

    fix: test

--- e2e/translatoins.test.ts
+++ e2e/translatoins.test.ts
@@ -3,5 +3,11 @@ import { test, expect } from '@playwright/test';
 test('self healing works with existing healing path', async ({ page }) => {
   await page.goto("http://localhost:4321/notebook/000004-tag-link");
   await page.getByText('Español').click();
+  // await page.getByText('English');
   expect(page.url()).toBe("http://localhost:4321/es/cuaderno/000004-image-tag-link");
+});
+test('self healing works with existing healing path in spanish', async ({ page }) => {
+  await page.goto("http://localhost:4321/es/cuaderno/000004-image-tag-link");
+  await page.getByText('English').click();
+  expect(page.url()).toBe("http://localhost:4321/notebook/000004-tag-link");
 });
