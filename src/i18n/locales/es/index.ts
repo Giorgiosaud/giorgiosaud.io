b21363fa3dec6b6e2744492db4aa33065195eee6
Jorge Saud <jorgelsaud@gmail.com>
2025-01-25 12:29:29 -0300

    fix: locales

--- src/i18n/locales/es/index.ts
+++ src/i18n/locales/es/index.ts
@@ -1,3 +1,7 @@
 export { default as nav } from "./nav.json"; 
 export { default as features } from "./features.json";
 export { default as hero } from "./hero.json";
+export { default as cta } from "./cta.json";
+export { default as badges } from "./badges.json";
+export { default as logos } from "./logos.json";
+export { default as contact } from "./contact.json";
