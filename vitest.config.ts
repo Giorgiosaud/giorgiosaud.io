e1bb51fac051883126f4e26d440dd2a0d8d489a5
Jorge Saud <jsaud@modyo.com>
2025-01-29 15:37:17 -0300

    fix: upgrade to tailwind 4

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,13 @@
 
 import path from 'path'
 import { getViteConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 const alias={
   '@': path.resolve(__dirname, './src'),
   '@firebase': path.resolve(__dirname, './src/firebase'),
   "@lib": path.resolve(__dirname, './src/lib'),
+  "@style": path.resolve(__dirname, './src/style'), 
   "@i18n": path.resolve(__dirname, './src/i18n'),
   "@utils": path.resolve(__dirname, './src/utils'),
   "@components": path.resolve(__dirname, './src/components'),
@@ -19,6 +21,9 @@ export default getViteConfig({
     environment: 'happy-dom',
     alias,
   },
+  plugins:[
+    tailwindcss(),
+  ],
   resolve: {
     alias,
   },
