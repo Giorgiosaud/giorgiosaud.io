e0aea6e3d28c5fae5d181f339064a61243b573f9
Jorge Saud <jsaud@modyo.com>
2025-07-02 00:20:51 -0400

    feat: add sendEmail action to handle email sending functionality

---
+++ src/actions/index.ts
@@ -0,0 +1,11 @@
+import { defineAction } from 'astro:actions';
+// import register from './register';
+import { sendEmail } from './sendEmail';
+// import signin from './signin';
+import { z } from 'astro:schema';
+
+export const server = {
+  // register,
+  // signin,
+  sendEmail
+}
