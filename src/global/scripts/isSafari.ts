bcf9376e62cd215dd4dda53b1fe5341f52a214c5
Jorge Saud <jsaud@modyo.com>
2025-07-02 20:59:08 -0400

    feat: add isSafari function to detect Safari browser

---
+++ src/global/scripts/isSafari.ts
@@ -0,0 +1,8 @@
+export function isSafari() {
+    const ua = navigator.userAgent;
+
+    // Exclude Chrome and Android
+    const isSafari = /^((?!chrome|android).)*safari/i.test(ua);
+
+    return isSafari;
+}
