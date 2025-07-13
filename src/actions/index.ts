ddf2bece859d859d3093a5c9154f80c6f05887f5
Jorge Saud <jsaud@modyo.com>
2025-07-13 14:16:44 -0400

    feat: implement contact form with email sending functionality; add localization support and remove old contact pages

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,8 +1,4 @@
-import { defineAction } from 'astro:actions';
-// import register from './register';
 import { sendEmail } from './sendEmail';
-// import signin from './signin';
-import { z } from 'astro:schema';
 
 export const server = {
   // register,
