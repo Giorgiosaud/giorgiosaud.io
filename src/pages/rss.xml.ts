2bfe1ddf979e44b4f752642dfb50449d177c60aa
Jorge Saud <jsaud@modyo.com>
2025-07-21 13:33:21 -0400

    feat: add RSS feed link to header and improve RSS feed generation for notes

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,17 +4,29 @@ import type { AstroSharedContext } from 'astro'
 
 export async function GET(context:AstroSharedContext) {
   const notes = await getCollection('notes')
-  return rss({
-    title: 'Notebook Posts',
-    description: 'My notebook Posts',
-    site: context.url.origin,
-    items: notes.map((note) => ({
+  const notas = await getCollection('notas')
+  const itemsNotes=notes.map((note) => ({
       title: note.data.title,
       pubDate: note.data.publishDate,
       description: note.data.resume,
       // Compute RSS link from post `id`
       // This example assumes all posts are rendered as `/blog/[id]` routes
       link: `/notebook/${note.id}/`,
-    })),
+    }));
+const itemNotas=notas.map((note) => ({
+        title: note.data.title,
+        pubDate: note.data.publishDate,
+        description: note.data.resume,
+        // Compute RSS link from post `id`
+        // This example assumes all posts are rendered as `/blog/[id]` routes
+        link: `/es/cuaderno/${note.id}/`,
+        }));
+  return rss({
+    title: 'Notebook Posts',
+    description: 'My notebook Posts',
+    site: context.url.origin,
+    items: [itemsNotes, itemNotas].flat(),
+    customData: `<language>en-us</language>`,
+    stylesheet: '/rss/styles.xsl',
   })
 }
