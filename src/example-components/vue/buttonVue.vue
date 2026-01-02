262fd4913919a4b598345876f8fcae94d269e7c5
Giorgio Saud <jsaud@modyo.com>
2026-01-02 06:09:05 -0600

    fix: correct CSS syntax and formatting in Badges and buttonVue components

--- src/example-components/vue/buttonVue.vue
+++ src/example-components/vue/buttonVue.vue
@@ -1,10 +1,10 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref } from "vue";
 
-const count = ref(0)
+const count = ref(0);
 onMounted(async () => {
-  setTimeout(() => count.value++, 1000)
-})
+  setTimeout(() => count.value++, 1000);
+});
 </script>
 
 <template>
@@ -12,27 +12,22 @@ onMounted(async () => {
     <h3>
       Vue Button was clicked {{ count }} {{ count === 1 ? "time" : "times" }}
     </h3>
-    <button
-      class="vue-button"
-      @click="count++"
-    >
-      Increment Count Vue
-    </button>
+    <button class="vue-button" @click="count++">Increment Count Vue</button>
   </div>
 </template>
 <style>
 h3,
- {
+button {
   font-family: Verdana, Arial, sans-serif;
 }
-.vue-button{
-  background: hsl( from green h s calc(l*1.1));;
+.vue-button {
+  background: hsl(from green h s calc(l * 1.1));
   color: white;
   font-weight: bold;
   padding: 0.5rem 1rem;
-  border-radius:10px;
-  &:hover{
-    background: hsl( from green h s calc(l*0.8));
+  border-radius: 10px;
+  &:hover {
+    background: hsl(from green h s calc(l * 0.8));
   }
 }
 .vue-component {
