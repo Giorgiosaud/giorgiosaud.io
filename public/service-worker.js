8c51c1ed712e262cd28770c0b38237372d540c34
Giorgio Saud <jsaud@modyo.com>
2026-01-04 17:37:03 -0600

    refactor(sw): remove caching, keep only push notification support

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,67 +1,20 @@
-// Name of the cache used in this version of the service worker.
-const PRECACHE = 'precache-v21'
+// Service worker for push notifications only
+// No caching - network requests go directly to server
 
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  './', // Alias for index.html
-]
-
-// The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches
-      .open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting()),
-  )
+  // Activate immediately
+  self.skipWaiting()
 })
 
-// The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', event => {
+  // Clean up any old caches from previous versions
   event.waitUntil(
-    caches
-      .keys()
-      .then(cacheNames => {
-        return cacheNames.filter(cacheName => cacheName !== PRECACHE)
-      })
-      .then(cachesToDelete => {
-        return Promise.all(
-          cachesToDelete.map(cacheToDelete => {
-            return caches.delete(cacheToDelete)
-          }),
-        )
-      })
-      .then(() => self.clients.claim()),
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => caches.delete(cacheName))
+      )
+    }).then(() => self.clients.claim())
   )
-})
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// Only GET requests can be cached - POST, PUT, DELETE etc are not cacheable.
-
-self.addEventListener('fetch', event => {
-  // Only handle same-origin GET requests
-  if (
-    event.request.url.startsWith(self.location.origin) &&
-    event.request.method === 'GET'
-  ) {
-    event.respondWith(
-      fetch(event.request)
-        .then(networkResponse => {
-          // Only cache successful responses
-          if (networkResponse && networkResponse.status === 200) {
-            const responseClone = networkResponse.clone()
-            caches.open(PRECACHE).then(cache => {
-              cache.put(event.request, responseClone)
-            })
-          }
-          return networkResponse
-        })
-        .catch(() => {
-          // If network fails, try to serve from cache
-          return caches.match(event.request)
-        }),
-    )
-  }
 })
 
 // Push notification received
